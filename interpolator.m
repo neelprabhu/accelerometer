8a42823efb4840a351d41688e500d8b1d5bcf1b7
neelprabhu <neelkprabhu@gmail.com>
2017-12-04 15:58:11 -0500

    Saving new results.

---
+++ interpolator.m
@@ -0,0 +1,29 @@
+% BME 271 Fall 2017
+% Tremor Reduction for Microsurgical Applications
+% interpolator: Master script that will allow training task and subsequent filt.
+% INPUTS:
+%   NONE
+% OUTPUTS:
+%   w
+
+function human = interpolator(human,xpath)
+
+discrep = size(human,1)-length(xpath);
+if discrep < 0
+    r     = randi(length(human(:,1))-1,[1 abs(discrep)]);
+    for i = 1:length(r)
+        if r(i) == 1
+           r(i) = 2;
+        end
+        xinterp = (human(r(i)-1,1) + human(r(i) + 1,1))./2;
+        yinterp = (human(r(i)-1,2) + human(r(i) + 1,2))./2;
+        interp  = [xinterp yinterp];
+        human   = [human(1:r(i),:); interp; human((r(i)+1):end,:)];
+    end  
+%     xpath = xpath(1:(end+discrep));
+%     ypath = ypath(1:(end+discrep));
+else
+    human = [human((discrep+1):end,1) human((discrep+1):end,2)];
+end
+
+end
---
+++ wFind.m
@@ -0,0 +1,54 @@
+% BME 271 Fall 2017
+% Tremor Reduction for Microsurgical Applications
+% wFind: Master script that will allow training task and subsequent filt.
+% INPUTS:
+%   NONE
+% OUTPUTS:
+%   w
+
+function [wx,wy,sx,sy] = wFind(human,xpath,ypath)
+
+human = human';
+tfx = diff(xpath);
+tfy = diff(ypath);
+
+rdfx = diff(human(1,:));
+rdfy = diff(human(2,:));
+
+rxcorr = xcorr(rdfx,rdfx,'biased');
+rycorr = xcorr(rdfy,rdfy,'biased');
+rxcorr = rxcorr(length(rdfx):end);
+rycorr = rycorr(length(rdfx):end);
+
+% Autocorrelation matrices (R)
+Rx = toeplitz(rxcorr,conj(rxcorr));
+Ry = toeplitz(rycorr,conj(rycorr));
+
+% Cross correlation vectors (p)
+px = xcorr(rdfx,tfx);
+py = xcorr(rdfy,tfy);
+px = px(1:length(rdfx))';
+py = py(1:length(rdfx))';
+
+% Calculate w
+u = ones(length(rdfx),1);
+invRx = inv(Rx);
+invRy = inv(Ry);
+
+numx = (1-(u'*invRx*px))*u;
+numy = (1-(u'*invRy*py))*u;
+
+denomx = u'*invRx*u;
+denomy = u'*invRy*u;
+
+qx = (numx./denomx) + px;
+qy = (numy./denomy) + py;
+
+wx = invRx*qx;
+wy = invRy*qy;
+
+sx = xcorr(human(1,:)',wx);
+sy = xcorr(human(2,:)',wy);
+sx = sx(1:length(wx));
+sy = sy(1:length(wy));
+end
